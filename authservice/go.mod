01c949b019717cbc3cad901bb1644f264e77ddb5
SageRiship <92663104+SageRiship@users.noreply.github.com>
2022-06-13 10:26:14 +0530

    Delete authservice directory

--- authservice/go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/SageRiship/vrcasino/authservice
-
-go 1.18
-
-require (
-	github.com/golang-jwt/jwt v3.2.2+incompatible
-	github.com/gorilla/mux v1.8.0
-)
--- authservice/model/userDetails.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type UserDetails struct {
-	Username string `json:"username,omitempty"`
-	Password string `json:"password,omitempty"`
-	Role     string `json:"role,omitempty"`
-}
